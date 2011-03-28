b6f16573f4c81350fbe2375c2ddcbc256cb2a9d6
Rogerio Atem de Carvalho <ratembr@gmail.com>
2011-03-28 00:28:59 -0300

    First (semi)complete version of Individual Credit Operation

--- bank_system/features/step_definitions/individual_credit_operation_steps.py
+++ bank_system/features/step_definitions/individual_credit_operation_steps.py
@@ -9,13 +9,12 @@ from bank_system.decorators.credit_analyst_decorator import CreditAnalystDecorat
 from bank_system.decorators.bank_account_decorator import BankAccountDecorator
 
 #It is not necessary to have a person and a machine, however they are created
-#to show the whole process of decoration and use
+#to show the process of using decorators
 @step(u'Given I am a registered Credit Analyst')
 def given_i_am_a_registered_credit_analyst(step):
-    world.person = Person()
-    world.person.name = 'John'
+    world.a_person = Person()
     world.credit_analyst = CreditAnalystDecorator('09876-5')
-    world.credit_analyst.decorate(world.person)
+    world.credit_analyst.decorate(world.a_person)
 
 @step(u'And an individual customer with account number (.+) asks for a personal loan')
 def and_an_individual_customer_with_account_number_account_number_asks_for_a_personal_loan(step, account_number):
@@ -37,8 +36,18 @@ def when_i_confirm_the_loan_request(step):
 def then_a_new_loan_request_with_the_account_number_and_desired_value_is_created(step, account_number, desired_value):
     world.an_individual_credit_operation = Process()
     world.loan_request_creation = Transformation()
+    world.loan_request_creation.set_operation(world.credit_analyst.create_loan_request)
+    world.loan_request_creation.operation |should| equal_to(world.credit_analyst.create_loan_request)
+    #associates the transformation to the process
+    world.an_individual_credit_operation.insert_movement(world.loan_request_creation)
+    world.an_individual_credit_operation.movements |should| contain(world.loan_request_creation)
+    #finally it runs the transformation
+    world.an_individual_credit_operation.movements[0].run(account_number, desired_value)
 
 @step(u'And the new loan request is associated to the Credit Analyst')
 def and_the_new_loan_request_is_associated_to_the_credit_analyst(step):
-    pass
+    #right now the only thing necessary is to have a person referenced by the process
+    #in fact, the test below is already done by unit level...
+    world.an_individual_credit_operation.insert_node(world.a_person)
+    world.an_individual_credit_operation.nodes |should| include(world.a_person)
 
