a5efac36d01536cfe94a9d155e76973b779b4c06
Rogerio Atem de Carvalho <ratembr@gmail.com>
2011-04-04 17:10:16 -0300

    Acceptance now complies to changes in Movements

--- bank_system/features/step_definitions/individual_credit_operation_steps.py
+++ bank_system/features/step_definitions/individual_credit_operation_steps.py
@@ -10,6 +10,7 @@ from bank_system.decorators.bank_account_decorator import BankAccountDecorator
 
 #Scenario Individual Customer asks for loan
 @step(u'Given I am a registered Credit Analyst')
+
 def given_i_am_a_registered_credit_analyst(step):
     world.a_person = Person()
     world.credit_analyst = CreditAnalystDecorator('09876-5')
@@ -33,16 +34,19 @@ def when_i_confirm_the_loan_request(step):
 
 @step(u'Then a new loan request with the (.+) and (.+) is created')
 def then_a_new_loan_request_with_the_account_number_and_desired_value_is_created(step, account_number, desired_value):
+    #Processes are nodes that use the company as the source node, and the client as destination
+    world.the_company = Machine()
+    world.a_client = Person()
     #now the business process starts to be assembled
-    world.an_individual_credit_operation = Process()
-    world.loan_request_creation = Transformation()
-    world.loan_request_creation.set_operation(world.credit_analyst.create_loan_request)
-    world.loan_request_creation.operation |should| equal_to(world.credit_analyst.create_loan_request)
+    world.an_individual_credit_operation = Process(world.the_company, world.a_client)
+    world.loan_request_creation = Transformation(world.credit_analyst.decorated, world.credit_analyst.decorated)
+    world.loan_request_creation.set_action(world.credit_analyst.create_loan_request)
+    world.loan_request_creation.action |should| equal_to(world.credit_analyst.create_loan_request)
     #associates the transformation to the process
     world.an_individual_credit_operation.insert_movement('loan request creation', world.loan_request_creation)
     world.an_individual_credit_operation.movements |should| contain('loan request creation')
     #finally it runs the transformation...
-    world.an_individual_credit_operation.movements['loan request creation'].run(world.account, desired_value)
+    world.an_individual_credit_operation.movements['loan request creation'].perform(world.account, desired_value)
     #checks if the loan request is stored in the Node's input_area
     world.a_person.input_area |should| contain(account_number)
 
@@ -64,14 +68,14 @@ def and_there_is_a_loan_request_of_account_account_number_to_be_analysed(step, a
 @step(u'When I pick to analyse the loan request of account (.+)')
 def when_i_pick_to_analyse_the_loan_request_of_account_account_number(step, account_number):
     #creates a new transformation to register the analysis
-    world.loan_request_analysis = Transformation()
+    world.loan_request_analysis = Transformation(world.credit_analyst.decorated, world.credit_analyst.decorated)
     #associates analyse operation to the transformation
-    world.loan_request_analysis.set_operation(world.credit_analyst.analyse)
+    world.loan_request_analysis.set_action(world.credit_analyst.analyse)
     #associates the transformation to the process
     world.an_individual_credit_operation.insert_movement('loan request analysis', world.loan_request_analysis)
     #finally it runs the transformation...
     #must refactor process.movements to make it easier to find operations => use a dictionary
-    world.an_individual_credit_operation.movements['loan request analysis'].run(world.account.number)
+    world.an_individual_credit_operation.movements['loan request analysis'].perform(world.account.number)
     #if everything is ok the loan request was stored in the Node's output_area
     world.a_person.output_area |should| contain(account_number)
 
@@ -83,7 +87,7 @@ def then_the_loan_request_has_the_decision_decision(step, decision):
 #Scenario Approved loan request
 @step(u'Given a loan request of value (.+) for account (.+) was approved')
 def given_a_loan_request_of_value_value_for_account_account_number_was_approved(step, value, account_number):
-    world.create_loan = Transformation()
+    world.create_loan = Transformation(world.credit_analyst.decorated, world.credit_analyst.decorated)
     #should remove the request from credit analyst area and send it to account area?
     #but it is already processed, should exist a log area in nodes?
     #world.create_loan.set_operation(world.credit_analyst.create_loan)
--- spec/movement_spec.py
+++ spec/movement_spec.py
@@ -9,8 +9,7 @@ class MovementSpec(unittest.TestCase):
     def it_checks_source_and_destination(self):
         a_node = Person()
         non_node = "I am not a Node"
-        #this also works...: Movement(source=a_node, destination=a_node) |should_not| throw(ContractError)
-        (Movement, a_node, a_node) |should_not| throw(ContractError)
-        (Movement,non_node, a_node) |should| throw(ContractError)
-        (Movement,a_node, non_node) |should| throw(ContractError)
+        (Movement, a_node,   a_node)   |should_not| throw(ContractError)
+        (Movement, non_node, a_node)   |should|     throw(ContractError)
+        (Movement, a_node,   non_node) |should|     throw(ContractError)
 
