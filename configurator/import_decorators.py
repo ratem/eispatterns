a16c4780deea66294d4bd7a2dcb9620c01643ac7
Rogerio Atem de Carvalho <ratembr@gmail.com>
2011-04-08 16:06:19 -0300

    Configurator works with ordinary modules or *import module*

---
+++ configurator/import_decorators.py
@@ -0,0 +1,3 @@
+from bank_system.decorators.credit_analyst_decorator import CreditAnalystDecorator
+from bank_system.decorators.bank_account_decorator import BankAccountDecorator
+
--- configurator/spec/rule_checker_spec.py
+++ configurator/spec/rule_checker_spec.py
@@ -4,6 +4,8 @@ from should_dsl import should
 from configurator.rule_checker import RuleChecker
 from domain.node.person import Person
 import bank_system.decorators
+import configurator.import_decorators
+
 
 class RuleCheckerSpec(unittest.TestCase):
 
@@ -12,11 +14,16 @@ class RuleCheckerSpec(unittest.TestCase):
         self.rule_checker = RuleChecker()
 
     def test_it_finds_decorators(self):
+        #it works with ordinary modules
         self.rule_checker.find_decorators(bank_system.decorators.bank_account_decorator)
         self.rule_checker.decorators |should| have(1).item
         self.rule_checker.decorators = []
         self.rule_checker.find_decorators(bank_system.decorators.credit_analyst_decorator)
-        #credit_analyst_decorator imports banlk_account_decorator => two decorators in the namespace
+         #credit_analyst_decorator imports bank_account_decorator => two decorators in the namespace
+        self.rule_checker.decorators |should| have(2).items
+        self.rule_checker.decorators = []
+        #it works with configurator.import_decorators, a import module
+        self.rule_checker.find_decorators(configurator.import_decorators)
         self.rule_checker.decorators |should| have(2).items
 
     def test_it_finds_decorators_rules(self):
