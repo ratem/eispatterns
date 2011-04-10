eb6e355dc7e7923d825ad40b951a8258137de503
Rogerio Atem de Carvalho <ratembr@gmail.com>
2011-04-10 16:01:25 -0300

    First nice version of RuleChecker, now storing broken rules, small improvements in spec

--- configurator/spec/rule_checker_spec.py
+++ configurator/spec/rule_checker_spec.py
@@ -13,36 +13,30 @@ class RuleCheckerSpec(unittest.TestCase):
         self.a_person = Person()
         self.rule_checker = RuleChecker()
 
-    def test_it_finds_decorators(self):
+    def it_finds_decorators(self):
         #it works with ordinary modules
         self.rule_checker.find_decorators(bank_system.decorators.bank_account_decorator)
-        self.rule_checker.decorators |should| have(1).item
+        self.rule_checker.decorators |should| have(1).decorator
         self.rule_checker.decorators = []
         self.rule_checker.find_decorators(bank_system.decorators.credit_analyst_decorator)
          #credit_analyst_decorator imports bank_account_decorator => two decorators in the namespace
-        self.rule_checker.decorators |should| have(2).items
+        self.rule_checker.decorators |should| have(2).decorators
         self.rule_checker.decorators = []
         #it works with configurator.import_decorators, a *import* module
         self.rule_checker.find_decorators(configurator.import_decorators)
-        self.rule_checker.decorators |should| have(2).items
+        self.rule_checker.decorators |should| have(2).decorators
 
-    def test_it_finds_decorators_rules(self):
+    def it_finds_decorators_rules(self):
         self.rule_checker.find_decorators(configurator.import_decorators)
         for decorator in self.rule_checker.decorators:
             self.rule_checker.find_rules(decorator)
         self.rule_checker.rules |should| have(2).rules
 
-    def test_it_check_rules(self):
+    def it_check_rules(self):
         self.rule_checker.find_decorators(configurator.import_decorators)
         for decorator in self.rule_checker.decorators:
             self.rule_checker.find_rules(decorator)
         self.rule_checker.check_rules(self.a_person)
         self.rule_checker.allowable_decorators |should| have(1).decorator
-        #non_allowable should contain tuples (class, reason), where reason is
-        #the __doc__ of a @rule == fine grained reasons for not decorating!
-
-'''
-if __name__ == '__main__':
-    unittest.main()
-'''
+        self.rule_checker.broken_rules |should| have(1).broken_rule
 
